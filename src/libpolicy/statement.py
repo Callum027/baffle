8a9396f35f9c5535a0a72ff192232639ba2d51dd
Callum Dickinson <callum@huttradio.co.nz>
2017-09-19 08:40:04 +1200

    Add some skeleton structure

---
+++ src/libpolicy/statement.py
@@ -0,0 +1,45 @@
+#
+# libpolicy - FAUCET-Bro network policy manager -- policy language
+# libpolicy/statement.py - statement parsing methods
+#
+# Copyright (c) 2017 Victoria University of Wellington
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+'''
+Statement parsing methods.
+'''
+
+
+class Statement(object):
+    '''
+    Policy language statement object.
+    '''
+
+
+    def write(data):
+        '''
+        Write this object repesentation of a statement to the output data.
+        '''
+        
+        pass
+
+
+def read(data):
+    '''
+    Read a statement, and return its object representation.
+    '''
+    
+    pass
